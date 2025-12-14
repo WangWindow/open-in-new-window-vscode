f29a22da8570c42e89f1b2dd7c2e756a37d6c913
WangWindow <1598593280@qq.com>
2025-12-14 14:57:09 +0800

    [update]: add some locale support.

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,19 @@
 import * as vscode from 'vscode';
+import * as l10n from '@vscode/l10n';
 
 export function activate(context: vscode.ExtensionContext) {
 	const openCommand = 'open-in-new-window.open';
 
 	let openCommandHandler = vscode.commands.registerCommand(openCommand, (uri?: vscode.Uri) => {
 		if (!uri) {
-			vscode.window.showWarningMessage('The intended usage is from the explorer context menu.');
+			vscode.window.showWarningMessage(l10n.t('command.open.warning'));
 			return;
 		}
 
-		const showNotification = vscode.workspace.getConfiguration("open-in-new-window").get("showNotification");
 
+		const showNotification = vscode.workspace.getConfiguration("open-in-new-window").get("showNotification");
 		if (showNotification) {
-			vscode.window.showInformationMessage(`Opening ${uri.path} in new window!`);
+			vscode.window.showInformationMessage(l10n.t('command.open.notification', uri.path));
 		}
 
 		vscode.commands.executeCommand('vscode.openFolder', uri, true);
