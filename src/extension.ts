d4fb05a50b8716101a02af150c2b8f6870817375
WangWindow <1598593280@qq.com>
2025-12-14 15:24:05 +0800

    [fix]: fix dependency errors

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,7 @@ import * as vscode from 'vscode';
 import * as l10n from '@vscode/l10n';
 
 export function activate(context: vscode.ExtensionContext) {
-	const openCommand = 'open-in-new-window.open';
-
+	const openCommand = 'quick-open-in-new-window.open';
 	let openCommandHandler = vscode.commands.registerCommand(openCommand, (uri?: vscode.Uri) => {
 		if (!uri) {
 			vscode.window.showWarningMessage(l10n.t('command.open.warning'));
